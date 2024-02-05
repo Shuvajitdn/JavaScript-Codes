6f41b8430bb59880a7511c64a20f2a558ba8d60d
Shuvajit Debnath <118671478+Shuvajitdn@users.noreply.github.com>
2024-02-05 12:12:21 +0530

    Create Strings.js

---
+++ Strings.js
@@ -0,0 +1,26 @@
+//Strings
+
+// let str = "ApnaCollege";
+// let str2 = 'Shuvajit'
+
+// console.log(str[1]);
+// console.log(str2[1]);
+// console.log(str2.length);
+
+//Template literals
+// let specialString =`This is a template literal`;
+// console.log(specialString);
+// console.log(typeof specialString);
+
+// let obj = {
+//     item: "pen",
+//     price: 10,
+// };
+
+// let output = `the cost of ${obj.item} is ${obj.price} rupees`;
+// console.log(output);
+
+let str = "Shuvajit \n Debnath";
+let str2 = "Shuvajit \t Debnath";
+console.log(str);
+console.log(str2);
